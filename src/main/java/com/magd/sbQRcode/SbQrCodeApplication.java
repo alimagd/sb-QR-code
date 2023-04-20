ce2c0dee222bffbc0102839001db52a801638f5f
AliMaGD <mailofmajidi@gmail.com>
2023-04-20 16:28:24 +0100

    a simple QR Generator and Controller to generate QR, just to test the version of google encoder which is declared its dependencies in pom file

---
+++ src/main/java/com/magd/sbQRcode/SbQrCodeApplication.java
@@ -0,0 +1,13 @@
+package com.magd.sbQRcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbQrCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbQrCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magd/sbQRcode/SbQrCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magd.sbQRcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbQrCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
